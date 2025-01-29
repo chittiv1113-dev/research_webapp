09e64380451ad51a95f8ebb2b189bf87d3eb367c
David Vanderklay <davidvanderklay@gmail.com>
2025-01-29 00:10:47 -0600

    update permissions of files, define project controller behavior

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,11 @@
 class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+    # Add filtering to this at some other point by keyword, department,, etc.
+  end
+
+  def show
+    @project = Project.find(params[:id])
+    # @tasks = @project.tasks
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,48 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2025_01_29_060430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "applications", force: :cascade do |t|
+    t.bigint "student_id", null: false
+    t.bigint "project_id", null: false
+    t.datetime "application_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["project_id"], name: "index_applications_on_project_id"
+    t.index ["student_id"], name: "index_applications_on_student_id"
+  end
+
+  create_table "faculties", force: :cascade do |t|
+    t.string "name"
+    t.string "department"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.bigint "faculty_id", null: false
+    t.boolean "is_active"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["faculty_id"], name: "index_projects_on_faculty_id"
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.integer "year"
+    t.string "major"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "applications", "projects"
+  add_foreign_key "applications", "students"
+  add_foreign_key "projects", "faculties"
 end
