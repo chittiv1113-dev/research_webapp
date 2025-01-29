a46cf147f5d9e42edeb39e9a59ecfc9aa14dedd6
David Vanderklay <davidvanderklay@gmail.com>
2025-01-29 00:02:34 -0600

    configure routes for different attributes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :faculties do
+    # Nested projects under the faculty
+    resources :projects
+  end
+  # Allow studnets to view projects
+  resources :projects, only: %i[index show]
+  # Applicatoins nested under students
+  resources :students do
+    resources :applications
+  end
+
+  # Root page set to projects index
+  root 'projects#index'
 end
