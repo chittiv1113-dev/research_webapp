7be29566be200ddf9b8bcd2b6750dec348aef758
David Vanderklay <142908365+davidvanderklay@users.noreply.github.com>
2025-02-10 00:50:55 -0600

    Revert "Oauth"

--- Gemfile
+++ Gemfile
@@ -1,7 +1,5 @@
 source "https://rubygems.org"
 
-
-ruby '~> 3.3.0'
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 8.0.1"
 # The modern asset pipeline for Rails [https://github.com/rails/propshaft]
@@ -20,12 +18,6 @@ gem "stimulus-rails"
 gem "jbuilder"
 gem "rexml"
 gem "rubocop"
-gem "dotenv-rails"
-
-gem "devise"
-gem "omniauth"
-gem "omniauth-google-oauth2"
-gem "omniauth-rails_csrf_protection"
 
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 # gem "bcrypt", "~> 3.1.7"
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_07_151846) do
+ActiveRecord::Schema[8.0].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
-  create_table "admins", force: :cascade do |t|
-    t.string "email", null: false
-    t.string "full_name"
-    t.string "uid"
-    t.string "avatar_url"
-    t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
-    t.index ["email"], name: "index_admins_on_email", unique: true
-  end
 end
