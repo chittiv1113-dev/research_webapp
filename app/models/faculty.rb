1617a277e9df374837c1bda31cbec8da84f6a0c5
Tam Pham <diamondlover32@gmail.com>
2025-02-26 21:58:55 +0000

    Fix failing faculty model tests

--- app/models/faculty.rb
+++ app/models/faculty.rb
@@ -1,6 +1,7 @@
 class Faculty < ApplicationRecord
   belongs_to :user
+  validates_associated :user
   has_and_belongs_to_many :projects, join_table: "project_faculties"
+
   validates :department, presence: true, length: { maximum: 255 } # Presence and max length for department
-  # validates :email, presence: true, length: { maximum: 255 } # Presence and max length for emailend
 end
