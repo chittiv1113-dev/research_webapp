9330eab99e1c7b5738c0acb5bca89072df5f47ce
maung888 <123016436+maung888@users.noreply.github.com>
2025-03-10 23:58:06 -0500

    Added Flash Script
    Added script to make the flash notice disappear after a few seconds

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,14 +2,4 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
-document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(function() {
-      let alert = document.querySelector(".alert");
-      if (alert) {
-        alert.style.transition = "opacity 0.5s";
-        alert.style.opacity = "0";
-        setTimeout(() => alert.remove(), 500);
-      }
-    }, 3000);
-  });
   
