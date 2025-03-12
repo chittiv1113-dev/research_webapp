ca7563fef5bed73f4df7d98a893279b079148e9d
Tam Pham <diamondlover32@gmail.com>
2025-03-12 04:58:29 +0000

    Setup Sidekiq with Heroku

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,6 @@
 Sidekiq.configure_server do |config|
   config.redis = {
-    url: ENV.fetch("REDIS_URL", "redis://localhost:6379/1")
+    url: ENV.fetch(ENV.fetch("REDIS_PROVIDER", "REDIS_URL"), "redis://localhost:6379/1"),
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }, # We must trust Heroku here
   }
 end
