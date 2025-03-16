ada45a3e35eb576af2ac6ef83f787a3ad598b401
David Vanderklay <davidvanderklay@gmail.com>
2025-03-16 00:18:42 -0500

    generate project mailer

---
+++ app/mailers/project_notification_mailer.rb
@@ -0,0 +1,2 @@
+class ProjectNotificationMailer < ApplicationMailer
+end
---
+++ spec/mailers/previews/project_notification_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/project_notification_mailer
+class ProjectNotificationMailerPreview < ActionMailer::Preview
+
+end
