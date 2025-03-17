988f0b39c8cedda1f30bd13077aa6057a6e7fb52
maung888 <123016436+maung888@users.noreply.github.com>
2025-03-17 02:20:47 -0500

    Stronger logic for search bar

--- app/mailers/form_mailer.rb
+++ app/mailers/form_mailer.rb
@@ -1,10 +1,9 @@
 class FormMailer < ApplicationMailer
-    default from: 'your-email@gmail.com' # Replace with your email
-  
+    default from: "your-email@gmail.com" # Replace with your email
+
     def send_form(form_link)
-      recipient_email = 'recipient@example.com' # Hardcoded email
+      recipient_email = "recipient@example.com" # Hardcoded email
       @form_link = form_link
-      mail(to: recipient_email, subject: 'New Form Submission')
+      mail(to: recipient_email, subject: "New Form Submission")
     end
-  end
-  
+end
