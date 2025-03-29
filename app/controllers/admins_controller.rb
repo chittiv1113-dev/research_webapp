37da6e6e11957fb33e081c432fcedded86e92ce9
David Vanderklay <davidvanderklay@gmail.com>
2025-03-29 16:28:48 -0500

    rubocop fixes admin auth

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,7 +1,7 @@
 
 class AdminsController < ApplicationController
   before_action :require_admin
-  
+
   def dashboard
     # This action will now only be accessible by admins.
     # No specific data loading needed for the view you provided.
