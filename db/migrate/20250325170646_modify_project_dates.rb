cf82461e724de1265fa6332a2242e6b382f10e28
maung888 <123016436+maung888@users.noreply.github.com>
2025-04-07 15:47:54 -0500

    Update 20250325170646_modify_project_dates.rb

--- db/migrate/20250325170646_modify_project_dates.rb
+++ db/migrate/20250325170646_modify_project_dates.rb
@@ -1,9 +1,10 @@
 class ModifyProjectDates < ActiveRecord::Migration[8.0]
   def change
-    remove_column :projects, :expires_at, :datetime
-    remove_column :projects, :start_semester, :string
-    remove_column :projects, :start_year, :integer
+      remove_column :projects, :expires_at, :datetime if column_exists?(:projects, :expires_at)
+      remove_column :projects, :start_semester, :string if column_exists?(:projects, :start_semester)
+      remove_column :projects, :start_year, :integer if column_exists?(:projects, :start_year)
     add_column :projects, :start_date, :date
     add_column :projects, :end_date, :date
   end
 end
+
